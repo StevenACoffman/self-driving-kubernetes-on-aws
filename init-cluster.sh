76be19c70cc744b8c7d14d3014b31f0b54c14d7f
Steve Coffman <steve.coffman@ithaka.org>
2019-06-08 12:00:35 -0400

    Fix yaml ending

--- init-cluster.sh
+++ init-cluster.sh
@@ -3,9 +3,9 @@
 AWS_DEFAULT_REGION="us-east-1"
 AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-${AWS_REGION:-"us-east-1"}}
 AWS_REGION=${AWS_DEFAULT_REGION}
-CLUSTER_NAME="$(eksctl get cluster --region=$AWS_REGION | sed -n 2p | awk '{print $1}')"
+export CLUSTER_NAME="$(eksctl get cluster --region=$AWS_REGION | sed -n 2p | awk '{print $1}')"
 
-export KUBECONFIG="$HOME/.kube/eksctl/clusters/${CLUSTER_NAME}.yaml"
+export KUBECONFIG="$HOME/.kube/eksctl/clusters/${CLUSTER_NAME}"
 
 AWS_PATH="/usr/local/bin"
 echo Initializing base cluster infrastructure for $CLUSTER_NAME in $AWS_REGION
