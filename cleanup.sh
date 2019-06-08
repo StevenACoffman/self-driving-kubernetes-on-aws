84212717114b31d4a3239e1061002aae7980f504
Steve Coffman <steve.coffman@ithaka.org>
2019-06-08 19:44:52 -0400

    Fix again

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,7 @@ AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-${AWS_REGION:-"us-east-1"}}
 AWS_REGION=${AWS_DEFAULT_REGION}
 export CLUSTER_NAME="$(eksctl get cluster --region=$AWS_REGION | sed -n 2p | awk '{print $1}')"
 
-export KUBECONFIG="$HOME/.kube/eksctl/clusters/${CLUSTER_NAME}.yaml"
+export KUBECONFIG="$HOME/.kube/eksctl/clusters/${CLUSTER_NAME}"
 
 export ASDF_KUBECTL_VERSION=1.12.7
 eksctl utils write-kubeconfig --name=${CLUSTER_NAME}
